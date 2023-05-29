f27e76314f737247d8a81e441d62bf3c01bc7e41
Stephanie d'Agata <stephanie.dagata@gmail.com>
2023-05-29 22:29:10 +0200

    Init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' 2022_TE_Overlap_CADC: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Stephanie D'AGATA \email{stephanie.dagata@gmail.com}
+#' 
+#' @date 2023/05/29
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
