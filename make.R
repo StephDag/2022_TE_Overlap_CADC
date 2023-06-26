3221c29074a184e4d44fbe37363636e45325fb80
Stephanie d'Agata <stephanie.dagata@gmail.com>
2023-06-26 16:51:38 +0200

    updated make file (certain r script on hold)

--- make.R
+++ make.R
@@ -28,4 +28,13 @@ devtools::load_all(here::here())
 ## Run Project ----
 
 # List all R scripts in a sequential order and using the following form:
-# source(here::here("analyses", "script_X.R"))
+source(here::here("analyses", "00_setup.R")) # setup libraries
+source(here::here("analyses", "001_Coastal_countries.R")) # list of coastal countries
+source(here::here("analyses", "01_IATA_DB.R")) # retrieve full database filtered by coastal countries
+source(here::here("analyses", "02_Clean_GPS.R")) # retrieve missing GPS locations and correct gps locations
+  # updates on the 26/06/2023
+  
+
+
+
+
