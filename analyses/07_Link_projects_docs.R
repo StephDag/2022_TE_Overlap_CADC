bd4778ef068dbfcdbd3f82b0cc46eaa5f13d8604
Stephanie d'Agata <stephanie.dagata@gmail.com>
2023-10-02 10:30:47 +0200

    updates script link to pdf, reorder scripts

---
+++ analyses/07_Link_projects_docs.R
@@ -0,0 +1,162 @@
+# Script to download pdf for each project which provided the direct link to pdf
+# Steph D'Agata
+# Sept 2023
+
+library(here)
+source(here("analyses","00_setup.R"))
+### linked to projects documents
+
+all.gps.sp.100m.100km.CADC <- readRDS(here("data","derived-data","iati_GPS_db_100km_100m_keywords_sectors.rds"))
+
+### List of url provided in the db
+#  uri
+url1 <- all.gps.sp.100m.100km.CADC %>% 
+  mutate(linked_data_uri = ifelse(linked_data_uri == "NA",NA,linked_data_uri)) %>%
+  filter(!is.na(linked_data_uri))
+dim(url1)
+as.factor(url1$linked_data_uri)
+
+# link to document
+url2 <- all.gps.sp.100m.100km.CADC %>% 
+  mutate(result_document_link_url = ifelse(result_document_link_url == "NA",NA,result_document_link_url)) %>%
+  filter(!is.na(result_document_link_url))
+dim(url2)
+
+# link to document
+url3 <- all.gps.sp.100m.100km.CADC %>% 
+  mutate(document_link_url = ifelse(document_link_url == "NA",NA,document_link_url)) %>%
+  filter(!is.na(document_link_url)) %>%
+  filter(str_detect(sector_narrative,"Fishing"))
+dim(url3)
+
+######## rbind all project with url documnents
+#all.gps.sp.100m.100km.CADC.url <- rbind(url1,url2)
+#all.gps.sp.100m.100km.CADC.url <- rbind(all.gps.sp.100m.100km.CADC.url,url3)
+
+######## focus only on columns with url information
+#all.gps.sp.100m.100km.CADC.url.simp <- all.gps.sp.100m.100km.CADC.url %>%
+#  select(iati_identifier,iati_identifier_bis,linked_data_uri,result_document_link_url,document_link_url) %>%
+#  distinct() %>%
+#  st_drop_geometry()
+
+######## separate the links
+#all.gps.sp.100m.100km.CADC.url.simp <- all.gps.sp.100m.100km.CADC.url.simp
+#  strsplit(html_string, "\\|")[[1]]
+
+######## separate the pdf link with |
+html_links_1 <- str_split(all.gps.sp.100m.100km.CADC.url.simp$linked_data_uri, "\\|"); names(html_links_1) <- all.gps.sp.100m.100km.CADC.url.simp$iati_identifier #; html_links_1 <- trimws(html_links_1)
+html_links_2 <- str_split(all.gps.sp.100m.100km.CADC.url.simp$result_document_link_url, "\\|") ; names(html_links_2) <- all.gps.sp.100m.100km.CADC.url.simp$iati_identifier #; html_links_2 <- trimws(html_links_2)
+html_links_3 <- str_split(all.gps.sp.100m.100km.CADC.url.simp$document_link_url, "\\|") ; names(html_links_3) <- all.gps.sp.100m.100km.CADC.url.simp$iati_identifier #; html_links_3 <- trimws(html_links_3)
+
+  # list to df
+html_links_1.df <- setNames(stack(html_links_1)[2:1], c('iati_identifier','url')) %>%
+  filter(!is.na(url)) %>% 
+  distinct()
+html_links_2.df <- setNames(stack(html_links_2)[2:1], c('iati_identifier','url')) %>%
+  filter(!is.na(url)) %>% 
+  distinct()
+html_links_3.df <- setNames(stack(html_links_3)[2:1], c('iati_identifier','url')) %>%
+  filter(!is.na(url)) %>% 
+  distinct()
+
+  # html link full
+rm(html_links_full)
+html_links_full <- left_join(html_links_1.df,html_links_2.df, by="iati_identifier",relationship = "many-to-many")
+html_links_full <- left_join(html_links_full,html_links_3.df, by="iati_identifier",relationship = "many-to-many")
+
+# pivot the dataframe into a long format: dataframe of pdf link to projects
+html_links_full_long <- pivot_longer(html_links_full, cols = c("url.x", "url.y", "url"), names_to = "url_type", values_to = "url_value") %>%
+  filter(!is.na(url_value)) %>%
+  select(-url_type) %>%
+  distinct() %>%
+  as.data.frame()
+
+# create a column to specify whether it is a pdf or not
+html_links_full_long <- html_links_full_long %>%
+  mutate(pdf = ifelse(str_detect(url_value,"pdf"),1,0)) %>%
+  mutate(pdf=as.factor(pdf)) %>%
+  filter(!is.na(url_value)) %>%
+  distinct()
+html_links_full_long$ID <- seq(1,dim(html_links_full_long)[1],1)
+
+library(rvest)
+library(purrr)
+library(tabulizer)
+library(rJava)
+
+# loop for download all pdf
+html_links_full_long.pdf <- html_links_full_long %>%
+  filter(pdf == 1)
+
+# loop to download all pdf provided for each project
+for (i in 1:dim(html_links_full_long.pdf)){
+ 
+#  for (i in 474:476){
+  print(paste("i=",i)) 
+  # pdf link
+  temp.page <- gsub(" ","",html_links_full_long.pdf[i,"url_value"]) # rm white space
+  
+  # change backlash to _
+  #filename <- gsub("/", "", temp.page)
+
+  # name and create our download destination folder
+  temp.proj.name <- gsub("-","_",html_links_full_long.pdf[i,"iati_identifier"])
+  temp.proj.name <- gsub(" ","",temp.proj.name)
+  temp.proj.name.2 <- paste0(temp.proj.name,"_",i,sep="")
+  
+  temp.pdf_dir <- here(paste0("outputs/",temp.proj.name,sep=""))
+  dir.create(temp.pdf_dir) # create directory
+  
+  # download pdf
+  #temp.destfile <- "/Users/stephanie/Documents/GitHub/2022_TE_Overlap_CADC/outputs/XM-DAC-41302-106878-1277-2019-ARG155_pdfs/ISR-Disclosable-P118979-12-22-2016-1482451254909.pdf"
+  #temp.destfile <- paste0(temp.pdf_dir,filename,sep="/")
+  
+  # Looks at page in firefox
+  #browseURL(temp.page) #, encodeIfNeeded = T, browser = "firefox.exe")
+  
+  # if error
+  skip_to_next <- FALSE
+  
+  # Saves pdf to "downloaded" folder if it exists
+  tryCatch(download.file(temp.page, path.expand(paste0(temp.pdf_dir,"/",temp.proj.name.2,".pdf",sep=""))), error = function(e) { skip_to_next <<- TRUE})
+  
+  if(skip_to_next) { next }
+  #download.file(url=temp.page,temp.destfile, mode = "wb")
+}
+
+################################################################################
+############# code when the link to html is provided, not a direct pdf link
+    # not finished - does not work
+
+# extract pdf from a html link (no specific pdf provided)
+# loop for download all pdf
+html_links_full_long.noPDF <- html_links_full_long %>%
+  filter(pdf == 0)
+
+i=1
+# extract all links from the page
+temp.page <- gsub(" ","",html_links_full_long.noPDF[i,"url_value"]) # rm white space
+
+links <- temp.page %>% 
+  html_nodes("a") %>% 
+  html_attr("href")
+
+library(downloader)
+download.file(page, pdf_dir)
+
+# filter the links to keep only the PDF documents
+pdf_links <- grep("\\.pdf$", links, value = TRUE)
+
+library(tidyverse)
+library(rvest)
+library(xml2)
+# scraping hyperlinks
+
+# Looks at page in firefox
+browseURL(links[1], encodeIfNeeded = T, browser = "firefox.exe")
+browseURL(links[2])
+
+# Saves pdf to "downloaded" folder if it exists
+download.file(full_links[1], path.expand("~/downloaded/teste.pdf"))
+
+
---
+++ analyses/On hold/03_projects_keywords.R
@@ -0,0 +1,134 @@
+# Categorized geographic projects based based on coastal, sustainability (CADC) and sustainable land-based projects
+# outputs: 3 terrestrial raster of 5km resolution each
+# Stephanie D'Agata
+# Sept 2023
+# Updates: Sept 2023
+library(here)
+source(here("analyses","00_setup.R"))
+
+# load data
+all.gps.sp.100m.100km <- readRDS(here("data","derived-data","iati_GPS_db_100km_100m.rds"))
+dim(all.gps.sp.100m.100km)
+
+# list of keywords
+# number of projects by key words
+# updates on the 29/09/2023
+key.marine <- c("\\bcoast","\\bmarin*","\\bsea","\\bocean*","\\bshore","\\bblue\\b","\\bbeach","\\bbmaritime","\\blittoral\\b","mangrove*","seagrass*","saltmarsh*","\\breef*\\b","kelp*",
+                "\\bestuar*","coral*","\\bbfish*","\\finfish*","\\bwave*","\\btidal","\\balgae","marsh*","\\baquac*","\\bmariculture", "\\bsubmarine","\\bdesalination","\\boffshore","\\bport",
+                "\\bseaweed*","\\bcrustacean*","\\bmollus*","\\bwetland*","\\btransitional water*","\\blagoon","\\bsalt pond*","bshell","\\bseamount*","\\bmid-ocean ridge*","\\bcentral gyre*","\\bdeep sea","\\bship*","\\bwater transport",
+                "\\bfinfish\\b", "\\bcontinental shelf*","\\bsand*","\\bnautical","\\batoll","\\bunderwater","\\bdemersal","\\baphotic", "pelagic*","neritic","surf","diver\\b", "divers\\b","dive\\b","SCUBA\\b","CPUE","\\bbivalve*","\\bboat*",
+                "\\bcaribbean biodiversity fund", "\\bclam", "\\bcockle", "\\bcrab", "\\bcruise","\\bdeep water", "\\bdesalini", "\\bdolphin", "\\bdredg", "\\bgrouper",
+                "\\bgulf", "\\bharbor", "\\bharbour", "\\binternational water", "\\blionfish", "\\bmpa", "\\bmussel", "\\bnaval", "\\boyster","\\bprawn", 
+                "\\bproblue", "\\bsnapper", "\\bsaltwater", "\\bsargassum", "\\bshark", "\\bshrimp", "\\btuna", "\\bturtle", "\\bunclos", "\\bvessel", "\\bwhale", "\\bwharf",
+                "\\bcôte\\b","\\bcôtière","\\bcôtier","\\bpêche","\\bpêcheur","\\bpêcherie","\\bocéan","\\bleue","\\bplage",  # french translation
+                "\\bmaritime","\\blittoral","\\bherbier","\\bmarais salant\\b","\\bmarée","\\bvague","\\balgues","\\bdessalement","\\btransport maritime\\b","\\beaux usées\\b","\\bruisselement","\\bsous-marin",
+                "\\bcrustacé*","\\bmollusque*","\\bzones humides","\\bzone humide","\\beaux de transition","\\blagon*","\\bétang salé","\\bétangs salés","\\bcoquillage*",
+                "dorsale médio-océanique","dorsales médio-océaniques","\\bgyre*","eaux profondes","eau profonde","gyre océanique","\\bbateau*","\\bpoisson*","\\bnautique*","\\bdémersa*","pélagique*","\\bplongée*\\b","\\bplongeur*\\b",
+                "\\bcapture*","coquillage*","crabe*","\\bcoque*","croisière*","mérou*","\\bdauphin*","\\bcrevette*","\\bsargasse*","requin*","thon*","tortue*","baleine*","\\bmoule*")
+
+key.sustainability <- c("\\bacidifica", "\\badapt", "\\badaptation", "\\bbio sphere","\\bbio-logical diversity","\\bbio-sphere","\\bbiodiversity", "\\bbio-diversity","\\bbiological diversity","\\bbiosphere",
+                        "\\bcarbon sink","\\bclimate change","\\bconserv","\\bdisaster risk reduction", "\\bDRR", "\\beco-system", "\\beco-touris", "\\becosystem", "\\becotouris", "\\benvironment*","\\becological",
+                        "\\bspecies","\\beutriphication", "\\bhabitat", "\\biuu", "\\bmarine debris ", "\\bmarine energy", "\\bmarine protection", "\\bmitigate", "\\bnature-based solutions",
+                        "capacity-build", "capacity build", "capacity develop", "\\bbadapt*","\\bocean health","\\bocean protection", "\\bvulnerab*", "tenure\\b", "TURF*", "territorial user rights",
+                        "\\boceanograph","\\boff-shore wind", "\\boffshore wind", "\\bover-fishing", "\\boverfishing","\\bclimat*",
+                        "\\boxygen", "\\bpreserv", "\\bprotected area", "\\brenewable", "\\bresearch", "\\bresilien","\\bresponse preparedness", "\\brestoration", "\\brestore", "\\brestoring", "\\bscience",
+                        "\\bscientifi", "\\bSDG 14", "\\bsea level rise", "\\bsea-level rise", "\\bsequestration", "\\bslr","\\bsustainab", "\\btidal energy", "\\bwildlife", "\\bwind energy","\\bbblue",
+                        "\\bbmpa","\\bblmma", "\\bboecm", "reserve*", "park*","no-take","natural monument","enclosure","closure","wilderness area*", "payment for ecosystem services", "PES", "\\bbMSC\\b","certification","eco-label", "ecotourism","protect*", ("communit((\\w+)(\\W+)){1,3}management\\b"), 
+                        "conserv((\\w+)(\\W+)){1,3}management\\b", ("communit((\\w+)(\\W+)){1,3}conserv"), ("communit((\\w+)(\\W+)){1,3}fish"), ("communit((\\w+)(\\W+)){1,3}natural resource"), 
+                        "comanagement\\b", "co-management\\b",
+                        "\\bbiodiversité", "\\bbio-diversité","\\diversité biologique","\\bpuit de carbone","\\bchangement climatique","\\bconserv","\\bdisaster risk reduction", "\\bDRR", "\\béco-system", "\\béco-touris", "\\bécosystem", "\\bécotouris", "\\benvironment*","\\bécologique",
+                        "\\bespèces","\\beutrophication", "\\bhabitat", "\\bdébris marin", "\\benergie marine", "\\bprotection marine", "\\bmitigate", "\\bsolutions fondées sur la nature", "\\bsolutions basées sur la nature",
+                        "renforcement de capacité", "développement de capacités", "\\bbadapt*","\\bprotection des océans", "\\bvulnerab*","droits territoriaux",
+                        "\\boceanograph","\\boff-shore wind", "\\béolien offshore","\\béolien en mer","\\bsurpeche",
+                        "\\boxygène", "\\bpreserv", "\\baire protégée", "\\brenouvelable", "\\brecherche", "\\brésilien","\\bpréparation à la réponse", "\\brestoration", "\\brestor*","\\bscience",
+                        "\\bscientifi", "\\bODD 14", "\\bmontée des eaux", "\\bsequestration","\\bdurab", "\\bénergie marémotrice", "\\bfaune", "\\bénergie éolienne","\\bbbleu",
+                        "\\bbamp", "reserve*", "parc*","monument naturel","fermeture","zone sauvage*", "paiement pour services écosystémiques", "PES", "\\bbMSC\\b","certification","eco-label", "ecotourism","protect*",
+                        ("communit((\\w+)(\\W+)){1,3}gestion\\b"), 
+                        "conserv((\\w+)(\\W+)){1,3}gestion\\b", ("communit((\\w+)(\\W+)){1,3}conserv"), ("communit((\\w+)(\\W+)){1,3}fish"), ("communit((\\w+)(\\W+)){1,3}natural resource"), 
+                        "comanagement\\b", "co-gestion\\b")
+
+key.land <- c("\\bwastewater","\\bwaste water","\\brunoff","\\bmarine debris",
+              "\\bmarine litter","\\bmarine plastic", "\\bmarine pollution","\\bocean debris", "\\bocean litter", "\\bocean plastic",
+              "\\bocean pollut","\\bwaste","\\bmarine pollut\\b","\\bwater treatment","\\bseawage","\\bnutrient pollution","water resources conservation","river basins development",
+              "\\beaux usees","\\beaux usees","\\brunoff","\\bdebris marins", # french
+              "\\bdechets marins","\\bplastique marin","\\bpollution marine","\\dechets marins","plastique oceanique",
+              "pollution des oceans","\\bdechets","pollution marine","traitement des eaux","eaux marines","pollution par les nutriments",
+              "conservation des ressources en eau"," développement des bassins fluviaux")
+
+
+############################
+#        COASTAL           #
+############################
+
+# new variable - 1/0 coastal
+all.gps.sp.100m.100km <- all.gps.sp.100m.100km %>%
+  mutate(Year = substring(activity_date_iso_date,1,4)) %>% # start year # 392946    257
+  filter(Year %in% seq(2000,2022,1)) %>%
+  mutate(coastal.nar = ifelse(str_detect(description_narrative,paste0(key.marine, collapse="|")),1,0)) %>%
+  mutate(coastal.title = ifelse(str_detect(title_narrative,paste0(key.marine, collapse="|")),1,0))
+
+all.gps.sp.100m.100km$coastal <- ifelse(all.gps.sp.100m.100km$coastal.nar == 1 | all.gps.sp.100m.100km$coastal.title == 1, 1, 0)
+all.gps.sp.100m.100km$coastal[is.na(all.gps.sp.100m.100km$coastal)] = 0 # keep the NAs in title or narrative, since it might be filtered through the OECD codes
+all.gps.sp.100m.100km <- all.gps.sp.100m.100km %>%
+  mutate(coastal = as.factor(coastal))
+
+summary(all.gps.sp.100m.100km$coastal) #         0:98451     1: 25260  
+
+###########################################
+#             SUSTAINABILITY              #
+###########################################
+
+# new variable - 1/0 sustainability
+all.gps.sp.100m.100km <- all.gps.sp.100m.100km %>%
+  mutate(Year = substring(activity_date_iso_date,1,4)) %>% # start year # 392946    257
+  filter(Year %in% seq(2000,2022,1)) %>%
+  mutate(sustainable.nar = ifelse(str_detect(description_narrative,paste0(key.sustainability, collapse="|")),1,0)) %>%
+  mutate(sustainable.title = ifelse(str_detect(title_narrative,paste0(key.sustainability, collapse="|")),1,0))
+
+all.gps.sp.100m.100km$sustainable <- ifelse(all.gps.sp.100m.100km$sustainable.nar == 1 | all.gps.sp.100m.100km$sustainable.title == 1, 1, 0)
+all.gps.sp.100m.100km$sustainable[is.na(all.gps.sp.100m.100km$sustainable)] = 0 # keep the NAs in title or narrative, since it might be filtered through the OECD codes
+all.gps.sp.100m.100km <- all.gps.sp.100m.100km %>%
+  mutate(sustainable = as.factor(sustainable))
+
+summary(all.gps.sp.100m.100km$sustainable) #      0: 62812      1: 60899 
+
+###########################################
+#        LAND-BASED           #
+###########################################
+
+# new variable - 1/0 land based
+all.gps.sp.100m.100km <- all.gps.sp.100m.100km %>%
+  mutate(Year = substring(activity_date_iso_date,1,4)) %>% # start year # 392946    257
+  filter(Year %in% seq(2000,2022,1)) %>%
+  mutate(land.nar = ifelse(str_detect(description_narrative,paste0(key.land, collapse="|")),1,0)) %>%
+  mutate(land.title = ifelse(str_detect(title_narrative,paste0(key.land, collapse="|")),1,0))
+
+all.gps.sp.100m.100km$land <- ifelse(all.gps.sp.100m.100km$land.nar == 1 | all.gps.sp.100m.100km$land.title == 1, 1, 0)
+all.gps.sp.100m.100km$land[is.na(all.gps.sp.100m.100km$land)] = 0 # keep the NAs in title or narrative, since it might be filtered through the OECD codes
+all.gps.sp.100m.100km <- all.gps.sp.100m.100km %>%
+  mutate(land = as.factor(land))
+
+summary(all.gps.sp.100m.100km$land) #      0: 121261      1: 2450
+
+
+#############################################
+#           add sector codes                #
+#############################################
+
+
+
+
+
+
+#############################################
+#           add sector codes                #
+#############################################
+##### estimate the 
+all.gps.sp.100m.100km.CADC <- all.gps.sp.100m.100km %>%
+  filter(coastal == 1 & sustainable == 1 & land == 0)
+length(unique(all.gps.sp.100m.100km.CADC$iati_identifier)) # 6722 projects, both coastal and sustainable and potentially land-based
+
+##### save the coded/sectors full db
+saveRDS(all.gps.sp.100m.100km.CADC,here("data","derived-data","iati_GPS_db_100km_100m_keywords_sectors.rds"))
+dim(all.gps.sp.100m.100km.CADC)
+
