618fb3a75a3e7f30bd87b552ecf401770381f0ee
Stephanie d'Agata <stephanie.dagata@gmail.com>
2024-01-23 11:06:40 +0100

    minor commit

--- R/NormMinMax.R
+++ R/NormMinMax.R
@@ -4,3 +4,12 @@ normalize <- function(x, ...) {
   return((x - min(x,na.rm = TRUE)) /(max(x,na.rm = TRUE) - min(x,na.rm = TRUE)))
 }
 
+# Function to be applied on the raster values; return: SpatRaster object
+rescale01 <- function(x) {
+  
+  val <- values(x)
+  
+  values(x) <- (val - min(val, na.rm = TRUE)) / (max(val, na.rm = TRUE) - min(val, na.rm = TRUE))
+  
+  x
+}
